eb99995b8212b9a5f6a3deeac01e5ea49dc4733d
Fredrik Holst <fredrik@eu.gpcregulatory.com>
2024-12-25 17:37:44 +0100

    added laptop config

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -2,29 +2,21 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, inputs, ... }:
+{ config, pkgs, ... }:
 
-# PLACEHOLDER FOR ACTUAL LAPTOP !!!!!!!!!!!!!!!!!
 
 {
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
       inputs.home-manager.nixosModules.default
+      ../../modules/bootloader/grub.nix
+      ../../modules/network/bluetooth.nix
+      ../../modules/dev-stack/dev-stack.nix
+      ../../modules/audio/audio.nix
+      ../../modules/graphics/compositor/wayland.nix
+      ../../modules/graphics/desktop/plasma.nix
     ];
-
-  # systemd-boot.
-  # boot.loader.systemd-boot.enable = true;
-  # boot.loader.efi.canTouchEfiVariables = true;
-
-  # grub stuff
-  boot.loader.systemd-boot.enable = false;
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "nodev";
-  boot.loader.grub.useOSProber = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.grub.efiSupport = true;  # Enable UEFI GRUB support
-  boot.loader.efi.efiSysMountPoint = "/boot/efi";
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
@@ -37,10 +29,6 @@
 
   # Enable networking
   networking.networkmanager.enable = true;
-
-  hardware.bluetooth.enable = true; # enables support for Bluetooth
-  hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
-
 
   # Set your time zone.
   time.timeZone = "Europe/Stockholm";
@@ -60,14 +48,6 @@
     LC_TIME = "sv_SE.UTF-8";
   };
 
-  # X11/Xorg stuff, trying disabled (no matter what i do the stupid middle button paste button is still there (im going to kill someone))
-  # services.xserver.enable = true;
-  services.displayManager.sddm.wayland.enable = true;
-
-  # Enables KDE my beloved (time to krash), and also sddm
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
-
   # Configure keymap in X11
   services.xserver.xkb = {
     layout = "se";
@@ -79,22 +59,6 @@
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
@@ -108,11 +72,11 @@
 
     ];
   };
-
+  
   home-manager = {
     extraSpecialArgs = { inherit inputs; };
     users = {
-      "user" = import /home/user/config/hosts/desktop/home.nix;
+      "user" = import ./home.nix;
     };
   };
 
@@ -136,79 +100,26 @@
   # $ nix search wget
  
   environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    nerdfonts
-    wget
-    btop
-    neovim
-    fastfetch
 
-    input-remapper
+    nerdfonts
     kdePackages.kate
     thunderbird
     steam
     vesktop
-    discord
     brave
     teamspeak3
-    postman
     mullvad-vpn
     kitty
-    docker
-    docker-client
 
-    # PHP stuff
-    php82
-    php84Packages.composer
-    laravel
-
-    nodejs
-    git
     vscodium
     vscode
-    mysql84
-    mariadb_114
     dbeaver-bin
     jetbrains.datagrip
     pgadmin4-desktopmode
 
   ];
-
-  fonts.packages = with pkgs; [
-  noto-fonts
-  noto-fonts-cjk-sans
-  noto-fonts-color-emoji
-  nerdfonts
-  ];
   
 
-  
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.11"; # Did you read the comment?
 
 }
--- hosts/laptop/hardware-configuration.nix
+++ hosts/laptop/hardware-configuration.nix
@@ -3,41 +3,41 @@
 # to /etc/nixos/configuration.nix instead.
 { config, lib, pkgs, modulesPath, ... }:
 
-# PLACEHOLDER FOR ACTUAL LAPTOP !!!!!!!!!!!!!!!!!
-
 {
   imports =
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "nvme" "ehci_pci" "xhci_pci" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/b078dc35-2552-433d-9952-e238bcab93d1";
+    { device = "/dev/disk/by-uuid/e7af399e-193c-43e1-8fea-2b76156782a9";
       fsType = "ext4";
     };
 
   fileSystems."/boot/efi" =
-    { device = "/dev/disk/by-uuid/C741-8BD0";
+    { device = "/dev/disk/by-uuid/4D1B-7A22";
       fsType = "vfat";
       options = [ "fmask=0077" "dmask=0077" ];
     };
 
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/0b970c9d-eadd-4130-8e7c-f6729ccee1fc"; }
-    ];
+  swapDevices = [ {
+    device = "/var/lib/swapfile";
+    size = 16*1024;
+    } ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp5s0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp2s0f0.useDHCP = lib.mkDefault true;
   # networking.interfaces.enp6s0.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp7s0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlp3s0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wwan0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
