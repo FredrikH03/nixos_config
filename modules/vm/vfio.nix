59f7b9e1c38d1a4d7ea499c258ec2ce59c6fb7c6
Fredrik Holst <fredrikholst03@hotmail.com>
2025-07-20 08:17:35 +0200

    vmification has begun

--- modules/vm/vfio.nix
+++ modules/vm/vfio.nix
@@ -6,4 +6,22 @@
   "iommu=pt"
   "vfio-pci.ids=1002:687f,1002:aaf8"
   ];
+
+  programs.virt-manager.enable = true;
+  virtualisation.spiceUSBRedirection.enable = true;
+  virtualisation.libvirtd = {
+    enable = true;
+    qemu = {
+      package = pkgs.qemu_kvm;
+      runAsRoot = true;
+      swtpm.enable = true;
+      ovmf = {
+        enable = true;
+        packages = [(pkgs.OVMF.override {
+          secureBoot = true;
+          tpmSupport = true;
+        }).fd];
+      };
+    };
+  };
 }
